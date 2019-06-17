f88caab60f206291d943c7184e6e995b28e638e2
Ingeborg FB <ingeborg.brovoll@gmail.com>
2019-06-17 12:15:51 +0200

    Hello World

--- testing_fromX/main.cpp
+++ testing_fromX/main.cpp
@@ -1,15 +1,11 @@
-//
-//  main.cpp
-//  testing_fromX
-//
-//  Created by Ingeborg Flatin Brovoll on 17/06/2019.
-//  Copyright © 2019 Ingebofb. All rights reserved.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+
+int main() {
+    
+    cout << "Hello World!\n";
+    
     return 0;
 }
